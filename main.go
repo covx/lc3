6482537c579ebbd4b68d4c81e669fe1a5c2da3b9
Maxim Chernyatevich <maxim.chernyatevich@gmail.com>
2020-05-26 12:21:02 +0300

    Improve description

--- main.go
+++ main.go
@@ -1,3 +1,9 @@
+// Copyright 2020 Maxim Chernyatevich. All rights reserved.
+// Use of this source code is governed by a GPLv3
+// license that can be found in the LICENSE file.
+
+// lc3 implements LC3 little computer assembly emulator
+
 package main
 
 import (
